67f12cb61aa0112931f98f4dc76771a10635eae3
GestioneCe <ruslick.ua@gmail.com>
2022-04-02 21:50:25 +0200

    Update webapp.js

--- resources/js/webapp.js
+++ resources/js/webapp.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     // Register service worker to control making site work offline
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('/gestioneCE/sw.js')
+            .register('/GestioneCE/sw.js')
             .then(function () { console.log('Service Worker Registered'); });
     }
     // Code to handle install prompt on desktop
